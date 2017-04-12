f6da6922b63ea82d7ea01c39214e6615431382d1
Grzegorz Witkowski <g.witkowski@ocado.com>
2017-04-12 14:31:45 +0200

    java

---
+++ src/main/java/localstack/test/Main.java
@@ -0,0 +1,83 @@
+package localstack.test;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.aws.messaging.config.annotation.EnableSqs;
+import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
+import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+@SpringBootApplication
+@EnableSqs
+@RestController
+public class Main {
+
+    @Autowired
+    private AmazonSQSAsync amazonSqs;
+
+    @Autowired
+    private QueueMessagingTemplate messagingTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @SqsListener("test-queue")
+    public void onMessage(TestMessage testMessage) {
+        System.out.println("test-queue received a new message: " + testMessage);
+    }
+
+    @RequestMapping("/list-queues")
+    public List<String> listQueues() {
+        return amazonSqs.listQueues().getQueueUrls();
+    }
+
+    @RequestMapping(path = "/messages", method = POST)
+    public void sendMessage(@RequestBody TestMessage testMessage) throws Exception {
+        String payload = objectMapper.writeValueAsString(testMessage);
+        MessageHeaderAccessor messageHeaderAccessor = new MessageHeaderAccessor();
+        messageHeaderAccessor.removeHeader("timestamp");
+        Message<String> message = MessageBuilder
+                        .withPayload(payload)
+                        .setHeaders(messageHeaderAccessor)
+                        .build();
+        messagingTemplate.send("test-queue", message);
+    }
+
+    @Bean
+    public AmazonSQSAsync amazonSQS() {
+        return AmazonSQSAsyncClientBuilder.standard()
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration("http://localhost:4576", "eu-west-1")
+                )
+                .withCredentials(
+                        new AWSStaticCredentialsProvider(new BasicAWSCredentials("test", "test"))
+                )
+                .build();
+    }
+
+    @Bean
+    public QueueMessagingTemplate messagingTemplate(AmazonSQSAsync amazonSqs) {
+        return new QueueMessagingTemplate(amazonSqs);
+    }
+}
---
+++ src/main/java/localstack/test/TestMessage.java
@@ -0,0 +1,23 @@
+package localstack.test;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class TestMessage {
+
+    private final String property1;
+
+    @JsonCreator
+    public TestMessage(@JsonProperty("property1") String property1) {
+        this.property1 = property1;
+    }
+
+    public String getProperty1() {
+        return property1;
+    }
+
+    @Override
+    public String toString() {
+        return "TestMessage{property1='" + property1 + "'}";
+    }
+}
